fddd9e489ee2bc589e3e7a12639b780361688cd8
AvlvZ <avlakov58@mail.ru>
2022-02-02 18:27:23 +0300

    First pages for MOTOTOUR project (v.2)

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const catalogsTour = document.querySelectorAll(".catalog__item"),
-        blockForCatalog = document.querySelectorAll(".catalog__block")
+        blockForCatalog = document.querySelectorAll(".catalog__block"),
+        footerPhone = document.querySelectorAll(".footer-copy__phone");
 
     catalogsTour.forEach((item, i) => {
         if(i % 2 === 1) {
             blockForCatalog[i].classList.add("right");
         }
-    })
+    });
 
 })
